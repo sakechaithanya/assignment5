1372f18f6af0466c3f55315b196c3222f80505cd
SAKE CHAITANYA <61873017+sakechaithanya@users.noreply.github.com>
2023-03-10 16:46:16 +0530

    Update assignment5.py

--- assignment5.py
+++ assignment5.py
@@ -1,3 +1,4 @@
+#1challenge
 class var:
     def __init__(self,x,y,z):
         self.x=x
@@ -11,3 +12,27 @@ class var:
         
 result=var(1,3,5)   
 print(result.sqsum())
+#2challenge
+class calulator:
+    def __init__(self,num1,num2):
+        self.num1=num1
+        self.num2=num2
+    def add(self):
+        return self.num1+self.num2
+    def subtract(self):
+        if self. num1>self.num2:
+           return  self.num1-self.num2
+        else:
+             return self.num2-self.num1
+    def multiply(self):
+        return  self.num1*self.num2
+    def div(self):
+        if self. num1>self.num2:
+           return  self.num1/self.num2
+        else:
+           return  self.num2/self.num1
+result=calulator(10,94)  
+print(result.add())
+print(result.subtract())
+print(result.multiply())
+print(result.div())         
